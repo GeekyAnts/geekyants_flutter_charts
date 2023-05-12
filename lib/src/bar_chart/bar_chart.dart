d1d7014f81e45b5e7979459a7cffe6309d795ef8
Gayathri-GA <gayathrid@geekyants.com>
2023-05-12 08:19:05 +0530

    Added mock implementation for getting the base view

--- lib/src/bar_chart/bar_chart.dart
+++ lib/src/bar_chart/bar_chart.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 import 'bar_chart_painter.dart';
 
-// TODO: To Add all required parameters and functions.
 class BarChart extends StatefulWidget {
   const BarChart({Key? key}) : super(key: key);
 
@@ -31,3 +30,111 @@ class RBarChartRenderer extends MultiChildRenderObjectWidget {
     super.updateRenderObject(context, renderObject);
   }
 }
+
+// TODO: For testing the mock logic for chart view uncommand below and run the code
+
+// class BarChart extends StatefulWidget {
+//   const BarChart({Key? key}) : super(key: key);
+
+//   @override
+//   State<BarChart> createState() => _BarChartState();
+// }
+
+// class _BarChartState extends State<BarChart> {
+//   @override
+//   Widget build(BuildContext context) {
+//     // return CustomPaint(
+//     //   painter: BarChartPainter(),
+//     //   size: Size(MediaQuery.of(context).size.width, 600),
+//     // );
+//   }
+// }
+
+// class RBarChartRenderer extends MultiChildRenderObjectWidget {
+//   const RBarChartRenderer({Key? key, required List<Widget> children})
+//       : super(key: key, children: children);
+
+//   @override
+//   RenderObject createRenderObject(BuildContext context) {
+//     return RenderRBarChart();
+//   }
+
+//   @override
+//   void updateRenderObject(BuildContext context, RenderRBarChart renderObject) {
+//     super.updateRenderObject(context, renderObject);
+//   }
+// }
+
+// class BarChartPainter extends CustomPainter {
+//   @override
+//   void paint(Canvas canvas, Size size) {
+//     final double chartWidth = size.width - 10.0 * 6;
+//     final double chartHeight = size.height;
+//     final double chartTop = (size.height - chartHeight) / 2;
+//     final double chartLeft = (size.width - chartWidth) / 2;
+//     const int numAxisPoints = 10;
+//     final double barWidth = chartWidth / numAxisPoints;
+
+//     final Paint axisPaint = Paint()
+//       ..color = Colors.grey
+//       ..strokeWidth = 0.5
+//       ..strokeCap = StrokeCap.round;
+
+//     // Draw x-axis points
+//     for (int i = 0; i <= numAxisPoints; i++) {
+//       final double x = chartLeft + i * barWidth;
+//       canvas.drawLine(
+//         Offset(x, chartTop + chartHeight + 5),
+//         Offset(x, chartTop),
+//         axisPaint,
+//       );
+//       TextSpan span = TextSpan(
+//         text: '${i * 0.5}',
+//         style: const TextStyle(color: Colors.black, fontSize: 12),
+//       );
+//       TextPainter tp = TextPainter(
+//         text: span,
+//         textAlign: TextAlign.center,
+//         textDirection: TextDirection.ltr,
+//       );
+//       tp.layout();
+
+//       // calculate position of ruler text
+//       double rulerTextTop = chartTop + chartHeight + 8;
+//       double rulerTextLeft = x - tp.width / 2;
+
+//       // adjust position of ruler text if it is too close to y-axis
+//       if (rulerTextLeft < chartLeft) {
+//         rulerTextLeft = chartLeft;
+//       }
+
+//       tp.paint(canvas, Offset(rulerTextLeft, rulerTextTop));
+//     }
+
+//     // Draw y-axis points
+//     for (int i = 0; i <= numAxisPoints; i++) {
+//       final double y = chartTop + chartHeight - i * chartHeight / numAxisPoints;
+//       canvas.drawLine(
+//         Offset(chartLeft - 5, y),
+//         Offset(chartLeft + chartWidth, y),
+//         axisPaint,
+//       );
+//       TextSpan span = TextSpan(
+//         text: '${i * 0.5}',
+//         style: const TextStyle(color: Colors.black, fontSize: 12),
+//       );
+//       TextPainter tp = TextPainter(
+//         text: span,
+//         textAlign: TextAlign.center,
+//         textDirection: TextDirection.ltr,
+//       );
+//       tp.layout();
+//       tp.paint(canvas, Offset(chartLeft - tp.width - 8, y - tp.height / 2));
+//     }
+//   }
+
+//   @override
+//   bool shouldRepaint(covariant CustomPainter oldDelegate) {
+//     return false;
+//   }
+// }
--- lib/src/bar_chart/bar_chart_axes.dart
+++ lib/src/bar_chart/bar_chart_axes.dart
@@ -1,46 +1,83 @@
 import 'package:flutter/material.dart';
 
-// TODO: To Add all required parameters and functions.
-
+/// A widget that represents the axes of a bar chart.
+///
+/// This widget is used to draw the horizontal and vertical axes of a bar chart.
+/// It takes several parameters that define the size and layout of the chart, as
+/// well as the number of axis points and the bar width.
 class BarChartAxes extends LeafRenderObjectWidget {
-  final double value;
+  /// The number of axis points to be displayed on both axes.
+  final int numAxisPoints;
+
+  /// The width of each bar in the chart.
+  final double barWidth;
+
+  /// The width of the chart.
+  final double chartWidth;
+
+  /// The height of the chart.
+  final double chartHeight;
+
+  /// The top margin of the chart.
+  final double chartTop;
+
+  /// The left margin of the chart.
+  final double chartLeft;
 
   const BarChartAxes({
-    super.key,
-    required this.value,
-  });
+    Key? key,
+    this.numAxisPoints = 10,
+    this.barWidth = 0.0,
+    this.chartWidth = double.infinity,
+    this.chartHeight = double.infinity,
+    this.chartTop = 0.0,
+    this.chartLeft = 0.0,
+  }) : super(key: key);
 
   @override
   RenderObject createRenderObject(BuildContext context) {
-    return RenderBarChartAxes(
-      value: value,
-    );
-  }
-
-  @override
-  void updateRenderObject(
-      BuildContext context, RenderBarChartAxes renderObject) {
-    renderObject.value = value;
+    return RenderBarChartAxes()
+      ..numAxisPoints = numAxisPoints
+      ..barWidth = barWidth
+      ..chartWidth = chartWidth
+      ..chartHeight = chartHeight
+      ..chartTop = chartTop
+      ..chartLeft = chartLeft;
   }
 }
 
+/// A render object that draws the axes of a bar chart.
+///
+/// This render object takes several parameters that define the size and layout of
+/// the chart, as well as the number of axis points and the bar width. It then draws
+/// the horizontal and vertical axes of the chart.
 class RenderBarChartAxes extends RenderBox {
-  double _value;
+  late int numAxisPoints;
+  late double barWidth;
+  late double chartWidth;
+  late double chartHeight;
+  late double chartTop;
+  late double chartLeft;
 
-  RenderBarChartAxes({
-    required double value,
-  }) : _value = value;
+  Paint axisPaint = Paint()
+    ..color = Colors.grey
+    ..strokeWidth = 0.5
+    ..strokeCap = StrokeCap.round;
 
-  double get value => _value;
-  set value(double value) {
-    if (_value == value) return;
-    _value = value;
-    markNeedsLayout();
-  }
+  RenderBarChartAxes()
+      : numAxisPoints = 10,
+        super();
 
   @override
   void performLayout() {
-    // Implement layout here
-    super.performLayout();
+    // Set the size and layout of the chart.
+    chartWidth = size.width - 10.0 * 6;
+    chartHeight = size.height;
+    chartTop = (size.height - chartHeight) / 2;
+    chartLeft = (size.width - chartWidth) / 2;
+    barWidth = chartWidth / numAxisPoints;
+
+    // Constrain the size of the render box to match the layout constraints.
+    size = constraints.constrain(Size(chartWidth, chartHeight));
   }
 }
