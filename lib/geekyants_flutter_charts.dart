5ee24c99a1db5b5515cb362c7a3efd057e5defe5
Gayathri-GA <gayathrid@geekyants.com>
2023-05-31 16:54:10 +0530

    Removed all unused widget files from the package folder

--- lib/geekyants_flutter_charts.dart
+++ lib/geekyants_flutter_charts.dart
@@ -2,5 +2,4 @@ library geekyants_flutter_charts;
 
 export 'src/bar_chart/bar_chart.dart';
 export 'src/bar_chart/bar_chart_label.dart';
-export 'src/bar_chart/bar_chart_axes.dart';
 export 'src/bar_chart/bar_chart_title.dart';
